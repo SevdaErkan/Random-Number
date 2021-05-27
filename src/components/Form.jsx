dbda91e7fb63179b90025cdc13cd697f8ed9912f
SevdaErkan <51799297+SevdaErkan@users.noreply.github.com>
2021-05-26 17:33:28 -0700

    Initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const Form = () => {
+   const [min, setMin] = useState("");
+   const [max, setMax] = useState("");
+   const [random, setRandom] = useState(0);
+
+   const randomNum = (min, max) => {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min)) + min;
+   };
+
+   return (
+      <div>
+         {random > 0 ? <div>{random}</div> : ""}
+         <form>
+            <label htmlFor="number">Lower Limit</label>
+            <input
+               type="number"
+               value={min}
+               onChange={(e) => setMin(e.target.value)}
+            />
+            <label htmlFor="number">Upper Limit</label>
+            <input
+               type="number"
+               value={max}
+               onChange={(e) => {
+                  setMax(e.target.value);
+               }}
+            />
+            <div id="buttons">
+               <button
+                  className="btns generate"
+                  onClick={(e) => {
+                     e.preventDefault();
+                     setRandom(randomNum(min, max));
+                  }}
+               >
+                  Generate
+               </button>
+               <button
+                  className="btns clear"
+                  onClick={(e) => {
+                     e.preventDefault();
+                     setRandom("");
+                     setMin("");
+                     setMax("");
+                  }}
+               >
+                  Clear
+               </button>
+            </div>
+         </form>
+      </div>
+   );
+};
+export default Form;
